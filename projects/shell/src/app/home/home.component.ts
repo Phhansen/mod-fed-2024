da7b4ddb26ffcdc7c189811e1ba7676e8a76deed
phhha <phhha@vestas.com>
2024-03-18 15:56:36 -0600

    feat: working dynamic plugins

--- projects/shell/src/app/home/home.component.ts
+++ projects/shell/src/app/home/home.component.ts
@@ -1,11 +1,25 @@
 import {Component, inject, OnInit, signal} from '@angular/core';
 import {WalletLibService} from 'wallet-lib';
+import {PluginOptions} from "../plugins/plugin";
+import {LookupService} from "../plugins/lookup.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   #walletService = inject(WalletLibService);
+  #lookupService = inject(LookupService);
+
   balance = this.#walletService.balance;
+  plugins: PluginOptions[] = [];
+  graphs: PluginOptions[] = [];
+
+  async ngOnInit() {
+    this.plugins = await this.#lookupService.lookup();
+  }
+
+  handleClick(graph: PluginOptions): void {
+    this.graphs.push(graph);
+  }
 }
