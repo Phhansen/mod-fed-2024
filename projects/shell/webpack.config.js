19c75449189088347c1b649c3b4f36cfd9ed2991
phhha <phhha@vestas.com>
2024-03-18 14:41:55 -0600

    feat: working module federation

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,9 +1,28 @@
+const {
+  share,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 module.exports = withModuleFederationPlugin({
-
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
-
+  shared: share({
+    "@angular/core": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/common": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/router": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/material": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+  }),
 });
-
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
