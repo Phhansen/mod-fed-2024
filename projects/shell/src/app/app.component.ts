325b66019320ca3dbef3023aeca0c5f0c41ab44b
phhha <phhha@vestas.com>
2024-03-18 14:52:57 -0600

    feat: working dynamic federation

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { getManifest } from '@angular-architects/module-federation';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomManifest, CustomRemoteConfig } from './utilities/remoteConfig';
+import { buildRoutes } from './utilities/routes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  #router = inject(Router);
   title = 'shell';
+  remotes: CustomRemoteConfig[] = [];
+
+  async ngOnInit(): Promise<void> {
+    const manifest = getManifest<CustomManifest>();
+    const routes = buildRoutes(manifest);
+    this.#router.resetConfig(routes);
+    this.remotes = Object.values(manifest);
+  }
 }
